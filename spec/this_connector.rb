68e44a3a466bad52a318ee532818f246030d3851
Matthew Billington <mbillington@gmail.com>
2026-05-28 13:58:40 +1000

    renamed connector.rb to this_connector

--- spec/this_connector.rb
+++ spec/this_connector.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 # This file defines a shared context for testing Workato connectors. 
-# It provides a way to load a connector and its settings from files, 
-# which can be used in multiple test cases to avoid duplication.
+# It defines the connector that will be tested against.
+# Ensure the connector file name matches the one used in the spec files (e.g., 'Kroki.rb' in this case).
 
 RSpec.shared_context 'connector' do
   let(:connector) { Workato::Connector::Sdk::Connector.from_file('Kroki.rb', settings) }
