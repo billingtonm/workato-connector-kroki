0c7c2f85594f4307e42c3544ab6aeda5bde5ae16
Matthew Billington <mbillington@gmail.com>
2026-06-07 11:40:55 +1000

    Shared testing files moved to to 'wisco' folder

--- spec/actions/generate_diagram_spec.rb
+++ spec/actions/generate_diagram_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative '../support/shared_examples'
+require_relative '../wisco/shared_examples'
 
 RSpec.describe 'actions/generate_diagram', :vcr do
   it_behaves_like 'a standard action', 'generate_diagram'
